be453820675798fb4520cb6b8cbbe95f41d5ccc7
khurshida03 <toshbekova.khurshidakhon@gmail.com>
2021-08-13 18:59:14 +0500

    changes

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/products_provider.dart';
+import 'package:shopping_app/screens/app_drawer.dart';
+import 'package:shopping_app/screens/edit_product_screen.dart';
+import 'package:shopping_app/widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your products'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, index) => Column(
+            children: [
+              UserProductItem(productsData.items[index].title,
+                  productsData.items[index].imageUrl),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
