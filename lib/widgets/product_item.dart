1bfbe9623396dc90a89010d459af7806a0b26a65
khurshida03 <toshbekova.khurshidakhon@gmail.com>
2021-08-13 18:49:44 +0500

    changes

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -43,6 +43,21 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Item added to cart',
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleitem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
